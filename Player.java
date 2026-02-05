9c501c68f2cc4ef424417279fc2e8ede5174225a
Kyle Schwartz <kyleschwartz12@gmail.com>
2026-02-05 16:11:32 -0600

    Testing to see if everything ported correctly

---
+++ Player.java
@@ -0,0 +1,16 @@
+
+public class Player
+{
+    char playerNum;
+    String playerName;
+    int score = 0;
+    boolean isPlayerColor = false;
+
+
+
+    public static void main(String[] args) 
+    {
+        
+    }
+    
+}
