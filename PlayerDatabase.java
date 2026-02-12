a5f2da17b1de209f3e8d63bd938862251576e2d5
RamonM18 <ramon.morales018@gmail.com>
2026-02-12 13:03:32 -0600

    final working code

--- PlayerDatabase.java
+++ PlayerDatabase.java
@@ -3,17 +3,17 @@ import java.sql.*;
 
 public class PlayerDatabase 
 {
-    // Connection details
+    //Connection details
     private static final String URL = "jdbc:postgresql://localhost:5432/photon";
     private static final String USER = "student";
     private static final String PASSWORD = "student";
     
-    // Method to connect to database
+    //Method to connect to database
     private Connection connect() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
     
-    // Method to get codename by player ID
+    //Method to get codename by player ID
     public String getCodename(int playerId) {
         String codename = null;
         String sql = "SELECT codename FROM players WHERE id = ?";
@@ -55,7 +55,7 @@ public class PlayerDatabase
             }
 
     }
-    // Test it
+    //testing the addplayer method
     public static void main(String[] args) {
         PlayerDatabase db = new PlayerDatabase();
 
@@ -63,7 +63,7 @@ public class PlayerDatabase
         String name = db.getCodename(1);  // Should return "Opus"
         System.out.println("Codename: " + name);
 
-        //testing add player
+        //manually testing to add player
         db.addPlayer(500,"Ramon");
 
         //print to show it was added
