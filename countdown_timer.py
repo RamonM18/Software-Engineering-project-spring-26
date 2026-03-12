94230dc839f4dfefacea23f11d7db2546ac69ec5
StevenHale11 <schale@uark.edu>
2026-03-12 13:50:41 -0500

    Clarify print statements in countdown_timer
    Added comments to clarify print statements in countdown_timer function.

--- countdown_timer.py
+++ countdown_timer.py
@@ -2,9 +2,10 @@ import time
 
 def countdown_timer():
     for i in range(30, 0, -1):
-        print(f"Game starts in {i} seconds")
+        print(f"Game starts in {i} seconds") #Print WHERE??? theres no terminal
         time.sleep(1)
+        #this is 30 nanon seconds factor of 1000
 
-    print("GAME START!")
+    print("GAME START!")# AGAIN where r u printing??????
 
 countdown_timer()
