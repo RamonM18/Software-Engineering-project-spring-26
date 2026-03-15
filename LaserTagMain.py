f73edf1af0b886bd0f9cb2d059aeccbd40ee0bbc
kdschw <kyleschwartz12@gmail.com>
2026-03-15 16:53:35 -0500

    Fix countdown timer flow

--- LaserTagMain.py
+++ LaserTagMain.py
@@ -206,7 +206,10 @@ class LaserTagMain:
         game_window.configure(bg="black")
         game_window.geometry("900x600")
 
-        game_window.protocol("WM_DELETE_WINDOW", self.root.destroy)
+        game_window.protocol(
+            "WM_DELETE_WINDOW",
+            lambda: [game_window.destroy(), self.root.deiconify()]
+        )
 
         score_frame = tk.Frame(game_window, bg="black")
         score_frame.pack(pady=10)
