770fd6b74df8945eb3a71741041bd05b6d521d7e
kdschw <kyleschwartz12@gmail.com>
2026-04-15 22:02:21 -0500

    Made changes

--- friendly_fire.py
+++ friendly_fire.py
@@ -12,12 +12,20 @@ def handle_hit(shooter, target):
     elif shooter.team_code == target.team_code:
         shooter.subtract_score(10)
         message = "FRIENDLY FIRE: " + shooter.get_player_name() + " hit teammate " + target.get_player_name()
-        
+
     else:
         shooter.add_score(10)
         message = shooter.get_player_name() + " hit " + target.get_player_name() + " (+10)"
 
     return message
+
+p1 = Player("Red1", 1)
+p2 = Player("Red2", 1)
+p3 = Player("Green1", 2)
+
+print(handle_hit(p1, p2))
+print(handle_hit(p1, p3))
+print(handle_hit(p1, None))
     
     
     
