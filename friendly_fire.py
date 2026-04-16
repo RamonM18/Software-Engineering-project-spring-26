a11485378917fda479a3be97b37ff1646089a78c
kdschw <kyleschwartz12@gmail.com>
2026-04-15 21:48:06 -0500

    Made changes

--- friendly_fire.py
+++ friendly_fire.py
@@ -1,14 +1,23 @@
 from player import Player
 
 def handle_hit(shooter, target):
-    if shooter is None or target is None:
-        return "Invalid hit"
-    if shooter.team_code == target.team_code:
-        shooter.subtract_score(10)  
-        return f"Friendy Fire: {shooter.get_player_name()} hit teammate {target.get_player_name()} (-10)"
+    message = ""
 
-    shooter.add_score(10)
-    return f"{shooter.get_player_name()} hit {target.get_player_name()} (+10)"
+    if shooter is None:
+        message = "Invalid hit"
+
+    elif target is None:
+        message = shooter.get_player_name() + " missed."
+
+    elif shooter.team_code == target.team_code:
+        shooter.subtract_score(10)
+        message = "FRIENDLY FIRE: " + shooter.get_player_name() + " hit teammate " + target.get_player_name()
+        
+    else:
+        shooter.add_score(10)
+        message = shooter.get_player_name() + " hit " + target.get_player_name() + " (+10)"
+
+    return message
     
     
     
